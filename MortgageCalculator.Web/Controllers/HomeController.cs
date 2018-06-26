4e13e6c692c14485b4a5de6661da501e910999f2
vivek6151 <32118032+vivek6151@users.noreply.github.com>
2018-06-26 23:48:56 +0530

    Add files via upload

---
+++ MortgageCalculator.Web/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Web.Mvc;
+using MortgageCalculator.Dto;
+using System.Threading.Tasks;
+
+namespace MortgageCalculator.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            string Baseurl = "http://localhost:49608/";
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(Baseurl);
+
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage httpResponseMsg = await client.GetAsync("api/mortgage");
+                //HttpResponseMessage httpResponseMsg = client.GetAsync("api/mortgage").GetAwaiter().GetResult();
+
+                IEnumerable<Mortgage> mortgagesList = null;
+                if (httpResponseMsg.IsSuccessStatusCode)
+                {
+                    var result = httpResponseMsg.Content.ReadAsStringAsync().Result;
+                    //var result = httpResponseMsg.Content.ReadAsStringAsync().GetAwaiter().GetResult();
+                    mortgagesList = JsonConvert.DeserializeObject<List<Mortgage>>(result);
+                    if (mortgagesList != null && mortgagesList.Count() > 0)
+                    {
+                        mortgagesList = mortgagesList.OrderBy(m => m.MortgageType).ThenBy(m => m.InterestRepayment);
+                    }
+                }
+                return View(mortgagesList);
+            }
+            //return View();
+        }
+
+    }
+}
---
+++ MortgageCalculator.Web/Scripts/script-custom-validator.js
@@ -0,0 +1,51 @@
+﻿$(document).ready(function () {
+    $('#mortgageForm').validate({
+        errorClass: 'help-block animation-slideDown',
+        errorElement: 'div',
+        errorPlacement: function (error, e) {
+            e.parents('.form-group > div').append(error);
+        },
+        highlight: function (e) {
+            $(e).closest('.form-group').removeClass('has-success has-error').addClass('has-error');
+            $(e).closest('.help-block').remove();
+        },
+        success: function (e) {
+            e.closest('.form-group').removeClass('has-success has-error');
+            e.closest('.help-block').remove();
+        },
+        rules: {
+            'txtLoanAmount': {
+                required: true,
+                digits: true
+            },
+
+            'txtInterestRate': {
+                required: true,
+                digits: true,
+                max: 24
+            },
+
+            'txtTermsInMonths': {
+                required: true,
+                digits: true,
+                max: 100
+            }
+        },
+        messages: {
+            'txtLoanAmount': {
+                required: 'Please enter Loan Amount',
+                digits: 'Please enter number only'
+            },
+
+            'txtInterestRate': {
+                required: 'Please enter Interest Rate',
+                digits: 'Please enter number only'
+            },
+
+            'txtTermsInMonths': {
+                required: 'Please enter period',
+                digits: 'Please enter number only'
+            }
+        }
+    });
+}); 
